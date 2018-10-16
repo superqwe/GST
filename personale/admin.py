625973b391a2f4ec9cd721e2a56ac4f4760895f3
HP <leonardo.masi@modomec.com>
2018-10-16 09:41:24 +0200

    crea lavoratore se non esiste

--- personale/admin.py
+++ personale/admin.py
@@ -1,19 +1,47 @@
+import os
+
 from django.contrib import admin
 
 from .models import Lavoratore
 
 
+def aggiorna_lavoratori(modeladmin, request, queryset):
+    path_base = r'C:\Users\HP\Desktop\Sicurezza2\Personale'
+
+    primo_ciclo = True
+    for root, dirs, files in os.walk(path_base):
+        path = root[len(path_base) + 1:]
+
+        if primo_ciclo:
+            for lavoratore in dirs:
+                lavoratore = lavoratore.strip().title().split()
+
+                if lavoratore[0] != 'Z':
+                    cognome, nome = lavoratore
+                    try:
+                        res = Lavoratore.objects.filter(cognome=cognome, nome=nome)[0]
+                    except IndexError:
+                        lavoratore = Lavoratore(cognome=cognome, nome=nome)
+                        lavoratore.save()
+
+            primo_ciclo = False
+
+
+aggiorna_lavoratori.short_description = "Aggiorna Lavoratori"
+
+
 class LavoratoreAdmin(admin.ModelAdmin):
-    fields = (('cognome', 'nome'), 'codice_fiscale')
+    actions = [aggiorna_lavoratori, ]
     list_display = ('cognome', 'nome',
                     'in_cantiere',
                     'rait', 'ci', 'idoneita', 'unilav',
-                    'art37','primo_soccorso','antincendio', 'preposto',
+                    'art37', 'primo_soccorso', 'antincendio', 'preposto',
                     'h2s', 'dpi3',
                     'carrello', 'ple', 'gru', 'imbracatore',
                     'spazi_confinati',
                     'rir', 'rspp')
-    pass
+
+    ordering = ['cognome', 'nome']
 
 
 admin.site.register(Lavoratore, LavoratoreAdmin)
--- personale/models.py
+++ personale/models.py
@@ -3,35 +3,38 @@ from django.db import models
 
 # Create your models here.
 class Lavoratore(models.Model):
-    in_cantiere =models.BooleanField(default=True)
+    in_cantiere = models.BooleanField(default=True, verbose_name='In Cantiere')
 
     cognome = models.CharField(max_length=50)
     nome = models.CharField(max_length=50)
-    codice_fiscale = models.CharField(max_length=16, null=True, blank=True)
+    codice_fiscale = models.CharField(max_length=16, null=True, blank=True, verbose_name='Codice Fiscale')
 
-    rait = models.DateField(null=True, blank=True)
-    ci = models.DateField(null=True, blank=True)
-    idoneita = models.DateField(null=True, blank=True)
-    unilav = models.DateField(null=True, blank=True)
+    rait = models.DateField(null=True, blank=True, verbose_name='Scadenza RAIT')
+    ci = models.DateField(null=True, blank=True, verbose_name="Documento d'Identità")
+    idoneita = models.DateField(null=True, blank=True, verbose_name='Idoneità')
+    unilav = models.DateField(null=True, blank=True, verbose_name='UNILAV')
 
     art37 = models.DateField(null=True, blank=True)
-    primo_soccorso = models.DateField(null=True, blank=True)
+    primo_soccorso = models.DateField(null=True, blank=True, verbose_name='Primo Soccorso')
     antincendio = models.DateField(null=True, blank=True)
     preposto = models.DateField(null=True, blank=True)
 
-    h2s = models.DateField(null=True, blank=True)
-    dpi3 = models.DateField(null=True, blank=True)
+    h2s = models.DateField(null=True, blank=True, verbose_name='H2S')
+    dpi3 = models.DateField(null=True, blank=True, verbose_name='DPI3')
 
-    carrello = models.DateField(null=True, blank=True)
-    ple = models.DateField(null=True, blank=True)
+    carrello = models.DateField(null=True, blank=True, verbose_name='Muletto')
+    ple = models.DateField(null=True, blank=True, verbose_name='PLE')
     gru = models.DateField(null=True, blank=True)
     imbracatore = models.DateField(null=True, blank=True)
 
-    spazi_confinati = models.DateField(null=True, blank=True)
+    spazi_confinati = models.DateField(null=True, blank=True, verbose_name='Spezi Confinati')
 
-    rir = models.DateField(null=True, blank=True)
-    rspp = models.DateField(null=True, blank=True)
+    rir = models.DateField(null=True, blank=True, verbose_name='RIR')
+    rspp = models.DateField(null=True, blank=True, verbose_name='RSPP')
+
+    def __str__(self):
+        return '%s %s' % (self.cognome, self.nome)
 
     class Meta:
-        verbose_name = 'Lavorattore'
-        verbose_name_plural = 'Lavorattori'
+        verbose_name = 'Lavoratore'
+        verbose_name_plural = 'Lavoratori'
