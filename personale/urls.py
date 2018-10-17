5f0c9980537f338ff316bb8be44bff1bee0b9661
HP <leonardo.masi@modomec.com>
2018-10-17 14:40:51 +0200

    bozza template globale

--- personale/urls.py
+++ personale/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /personale/
     path('', views.index, name='index'),
+    # /personale/globale/
+    path('globale/', views.globale, name='globale'),
+    # /personale/scadenza/
+    path('scadenza/', views.scadenza, name='scadenza'),
 ]
--- personale/views.py
+++ personale/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from personale.models import Lavoratore
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the ''personale'' index.")
 
+
+def globale(request):
+    lavoratori = Lavoratore.objects.order_by('cognome', 'nome')
+
+    template = loader.get_template('personale/index.html')
+    context = {
+        'lavoratori': lavoratori,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def scadenza(request):
+    return HttpResponse("Hello, world. You're at the ''personale'' index.")
