e7c52c4f5c51296e7c644c83f57d000d7d748c0d
leonardo.masi <leonardo.masi@modomec.org>
2019-11-04 13:59:43 +0100

    aggiorna_unilav: bozza assunzione

--- personale/views_aggiorna_unilav.py
+++ personale/views_aggiorna_unilav.py
@@ -1,23 +1,36 @@
+import os
 from pprint import pprint as pp
 
 from django.core.exceptions import ObjectDoesNotExist
 
 from personale.models import Azienda, Lavoratore, Cantiere
 
+def rinomina_unilav(lavoratore):
+    cartella_iniziale = os.getcwd()
+    cartella_personale = 'C:\Users\leonardo.masi\Documents\Personale'
+
 
 def assunzione(nominativi):
     errori = []
+    print('\nAssunzione')
     for nominativo in nominativi:
         cognome, nome, cf, data_assunzione, data_scadenza = nominativo
 
         try:
             lavoratore = Lavoratore.objects.get(cognome=cognome, nome=nome)
-            lavoratore.codice_fiscale = cf
-            lavoratore.data_assunzione = data_assunzione
-            lavoratore.unilav = data_scadenza
-            lavoratore.save()
+            print(lavoratore)
         except ObjectDoesNotExist:
-            print('errore', nominativo)
-            errori.append(('%s %s' % (cognome, nome), 'Lavoratore non trovato'))
+            lavoratore = Lavoratore(cognome=cognome, nome=nome)
+            lavoratore.save()
+            print(lavoratore, '---> nuovo')
+
+        lavoratore.codice_fiscale = cf
+        lavoratore.data_assunzione = data_assunzione
+        lavoratore.unilav = data_scadenza
+        lavoratore.in_forza = True
+        lavoratore.azienda = Azienda.objects.get(nome='Modomec')
+        lavoratore.save()
+
+        rinomina_unilav(lavoratore)
 
     return errori
