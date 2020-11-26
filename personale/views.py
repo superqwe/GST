032cc2fd69521ccd5e7d028e0cb4c2ff3bac2a79
leonardo.masi <leonardo.masi@modomec.org>
2020-11-26 15:14:28 +0100

    simulazioni_emergenze: tabella suddivisione per cantieri

--- personale/views.py
+++ personale/views.py
@@ -493,29 +493,43 @@ def tesserini(request):
 def simulazione_emergenze(request):
     simulazioni = Simulazione_Emergenza.objects.all()
 
-    presenze = [len(Lavoratore.objects.filter(simulazione_emergenza__data=simulazione.data)) for simulazione in
-                simulazioni]
+    presenze_totali = [len(Lavoratore.objects.filter(simulazione_emergenza__data=simulazione.data)) for simulazione in
+                       simulazioni]
 
     lavoratori = Lavoratore.objects.filter(in_forza=True, azienda=Azienda.objects.get(nome='Modomec')).order_by(
         'cantiere', 'cognome')
 
     matrice_simulazioni = []
     stato_lavoratore = []
+    presenze_per_cantiere = {}
     for lavoratore in lavoratori:
         simulazioni_lavoratore = lavoratore.simulazione_emergenza_set.all()
 
         rigo = [simulazione in simulazioni_lavoratore for simulazione in simulazioni]
         matrice_simulazioni.append(rigo)
 
-        stato_lavoratore.append(any(rigo))
+        stato = any(rigo)
+        stato_lavoratore.append(stato)
+
+        if lavoratore.cantiere not in presenze_per_cantiere:
+            presenze_per_cantiere[lavoratore.cantiere] = [0, 0]
+
+        if stato:
+            presenze_per_cantiere[lavoratore.cantiere][0] += 1
+        else:
+            presenze_per_cantiere[lavoratore.cantiere][1] += 1
+
+    for cantiere, presenza_cantiere in presenze_per_cantiere.items():
+        percentuale_presenza = 100 * presenza_cantiere[0] / sum(presenza_cantiere)
+        presenze_per_cantiere[cantiere].append(percentuale_presenza)
 
     totali_lavoratori = stato_lavoratore.count(True), stato_lavoratore.count(False), '%.0f' % (
             stato_lavoratore.count(True) / (stato_lavoratore.count(True) + stato_lavoratore.count(False)) * 100)
 
     context = {'lavoratori': lavoratori,
                'matrice_simulazioni': zip(lavoratori, matrice_simulazioni, stato_lavoratore),
-               'presenze': presenze,
-               'simulazioni': zip(simulazioni, presenze),
+               'presenze_per_cantiere': presenze_per_cantiere,
+               'simulazioni': zip(simulazioni, presenze_totali),
                'totali_lavoratori': totali_lavoratori,
                }
 
